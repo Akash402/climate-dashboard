6c79306ffd791bee39b9793951b353f777afdbb2
Akash Gujarathi <akash6513@gmail.com>
2025-09-07 14:43:44 +0100

    Removed all tests

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Test suite for Climate Dashboard.
-
-This package contains unit tests for all modules in the climate dashboard.
-"""
--- tests/ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-UI tests for Climate Dashboard using Playwright.
-
-This package contains end-to-end UI tests that verify the complete
-user experience of the climate dashboard.
-"""
